f1bae3e43ea1e4b2b5437feec31ffcc23def67fc
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2018-01-30 14:17:20 -0800

    processing updated
    issue: doesn't identify neighbors correctly

--- game_of_life.py
+++ game_of_life.py
@@ -1,4 +1,5 @@
 import gol_display
+import random
 
 # class for a cell
 class Cell:
@@ -14,6 +15,9 @@ class Cell:
 	def die(self):
 		self.alive = 0
 
+	def live(self):
+		self.alive = 1
+
 class Table:
 	cells = []
 
@@ -25,42 +29,101 @@ class Table:
 		for i in range(0, table_size_x):
 			cell_row = []
 			for j in range(0, table_size_y):
-				cell_row.append(Cell(i, j, 0))\
+				cell_row.append(Cell(i, j, random.randint(0,1)))\
 
 			self.cells.append(cell_row)
 
 
 	def neighbors(self, cell):
 		neighbors = []
+		try:
+			neighbors.append(self.cells[cell.pos_x][cell.pos_y+1])
+		except IndexError:
+			None
 
-		neighbors.append(cells[cell.pos_x][cell.pos_y+1])
-		neighbors.append(cells[cell.pos_x+1][cell.pos_y+1])
-		neighbors.append(cells[cell.pos_x+1][cell.pos_y])
-		neighbors.append(cells[cell.pos_x+1][cell.pos_y-1])
-		neighbors.append(cells[cell.pos_x][cell.pos_y-1])
-		neighbors.append(cells[cell.pos_x-1][cell.pos_y-1])
-		neighbors.append(cells[cell.pos_x-1][cell.pos_y])
-		neighbors.append(cells[cell.pos_x-1][cell.pos_y+1])
+		try:	
+			neighbors.append(self.cells[cell.pos_x+1][cell.pos_y+1])
+		except IndexError:
+			None
+
+		try:
+			neighbors.append(self.cells[cell.pos_x+1][cell.pos_y])
+		except IndexError:
+			None
+
+		try:	
+			neighbors.append(self.cells[cell.pos_x+1][cell.pos_y-1])
+		except IndexError:
+			None
+	
+		try:
+			neighbors.append(self.cells[cell.pos_x][cell.pos_y-1])
+		except IndexError:
+			None
+	
+		try:
+			neighbors.append(self.cells[cell.pos_x-1][cell.pos_y-1])
+		except IndexError:
+			None
+
+		try:
+			neighbors.append(self.cells[cell.pos_x-1][cell.pos_y])
+		except IndexError:
+			None
+	
+		try:
+			neighbors.append(self.cells[cell.pos_x-1][cell.pos_y+1])
+		except IndexError:
+			None
 
 		return neighbors
 
 	def checkCell(self, cell):
 		neighbors = self.neighbors(cell)
+		print(str(len(neighbors))+": ", end="")
 		result = 0
+		neighbors_alive = 0 
 		#underpopulation, normal, overpopulation, reproduction
-		for neighbor in neighbors
+		for neighbor in neighbors:
+			if neighbor.alive == 1:
+				neighbors_alive += 1
+
+		print(str(neighbors_alive) + ", " + str(result))		
+
+		if neighbors_alive < 2:
+			result = 0
+		elif neighbors_alive > 3:
+			result = 2
+		else:
+			result = 1
+			if neighbors_alive == 3 and cell.alive == 0:
+				result = 3
+
+
+		return result
+
 
 	def process(self):
 		for row in self.cells:
 			for cell in row:
+				result = self.checkCell(cell)
+				if cell.alive == 1:
+					if result == 0 or result == 2:
+						cell.die()
+				else:
+					if result == 3:
+						cell.live()
 
 
 
-table_size_x = 10
-table_size_y = 10
+table_size_x = 3
+table_size_y = 3
 
 table = Table(table_size_x, table_size_y)
 
+gol_display.printTable(table)
+input("Press Enter to continue...")
+table.process()
 gol_display.printTable(table)
 
 
