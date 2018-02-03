c8b8aaf0267ec729884c21cb3aa9cec6ebc3942c
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2018-02-02 17:31:09 -0800

    added third, final exercise - calculator

--- gui_test.py
+++ gui_test.py
@@ -54,8 +54,67 @@ class GuiEx2:
 		self.label_index %= len(self.LABEL_TEXT)
 		self.label_text.set(self.LABEL_TEXT[self.label_index])
 
+class GuiEx3Calculator:
+	def __init__(self, master):
+		self.master = master
+		master.title("Calculator")
+
+		self.total = 0
+		self.entered_number = 0
+
+		self.total_label_text = IntVar()
+		self.total_label_text.set(self.total)
+		self.total_label = Label(master, textvariable=self.total_label_text)
+
+		self.label = Label(master, text="Total:")
+
+		# wrap the command with Tk register
+		vcmd = master.register(self.validate)
+		# validate - validates the contents (with the validatecommand method): focusin, focusout, key - events
+		# validatecommand - pass a tuple - %P
+		self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+
+		# wrap update functions in lambda funtions (to spcify which parameters to pass)
+		self.add_button = Button(master, text="+", command=lambda: self.update("add"))
+		self.subtract_button = Button(master, text='-', command=lambda: self.update("subtract"))
+		self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
+
+		# LAYOUT
+		self.label.grid(row=0, column=0, sticky=W)
+		self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
+
+		self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
+
+		self.add_button.grid(row=2, column=0)
+		self.subtract_button.grid(row=2, column=1)
+		self.reset_button.grid(row=2, column=2, sticky=W+E)
+
+	def validate(self, new_text):
+		if not new_text:
+			self.entered_number = 0
+			return True
+
+		try:
+			self.entered_number = int(new_text)
+			return True
+		except ValueError:
+			return False
+
+	def update(self, method):
+		if method == "add":
+			self.total += self.entered_number
+		elif method == "subtract":
+			self.total -= self.entered_number
+		else:
+			self.total = 0
+
+		self.total_label_text.set(self.total)
+		# clear the entry widget from first index (0) to END; also deletion triggers validation - updates entered_number to 0
+		self.entry.delete(0, END)
+
+
 root = Tk()
-guiEx2 = GuiEx2(root)
+guiEx3Calculator = GuiEx3Calculator(root)
 # starts looping to accept events from the window
 root.mainloop()
 
@@ -80,4 +139,13 @@ root.mainloop()
 	place() - explicit position and size (pixels) - not recommended"""
 
 """ Custom events.
-	bind() method - in every widget class"""
+	bind() method - in every widget class
+	events - <Button-1> - LMK, <Button-2> - MMK, <Button-3> - RMK
+	<ButtonRelease-1> <B1-Motion>
+	<Enter> and <Leave> - mouse cursor entered or left widget
+	<Key> - keyboard key, also specific key, e.g. <Return> <Shift-Up>
+	<Configure> - widget changed size"""
+
+""" StringVar, IntVar, etc. - tkinter variable wrapper - e.g. for dynamic text etc."""
+
+""" 
