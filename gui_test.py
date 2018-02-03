768da49b3d3caeffe3bf4ff827105640b137222f
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2018-02-02 16:54:51 -0800

    finished second ex, with interactive label

--- gui_test.py
+++ gui_test.py
@@ -15,7 +15,7 @@ class GuiEx1:
 		self.greet_button.pack()
 
 		# create abutton root.quit - Tk function to quit
-		self.close_button = Button(master, text='Close', command=root.quit)
+		self.close_button = Button(master, text='Close', command=master.quit)
 		self.close_button.pack()
 
 	def greet():
@@ -23,20 +23,39 @@ class GuiEx1:
 
 class GuiEx2:
 
-	LABEL_TEXT
+	LABEL_TEXT = ["This is our first GUI!",
+       				"Actually, this is our second GUI.",
+       				"We made it more interesting...",
+        			"...by making this label interactive.",
+        			"Go on, click on it again."]
 
 	def __init__(self, master):
 		self.master = master
 		master.title("A simple GUI")
 
 		self.label_index = 0
-		self.label_text = StringVal()
+		self.label_text = StringVar()
 		self.label_text.set(self.LABEL_TEXT[self.label_index])
 		self.label = Label(master, textvariable=self.label_text)
+		self.label.bind("<Button-1>", self.cycle_label_text)
 		self.label.pack()
 
+		self.greet_button = Button(master, text='Greet', command=self.greet)
+		self.greet_button.pack()
+
+		self.close_button = Button(master, text='Close', command=master.quit)
+		self.close_button.pack()
+
+	def greet(self):
+		print('Greetings!')
+
+	def cycle_label_text(self, event):
+		self.label_index += 1
+		self.label_index %= len(self.LABEL_TEXT)
+		self.label_text.set(self.LABEL_TEXT[self.label_index])
+
 root = Tk()
-guiEx1 = GuiEx1(root)
+guiEx2 = GuiEx2(root)
 # starts looping to accept events from the window
 root.mainloop()
 
